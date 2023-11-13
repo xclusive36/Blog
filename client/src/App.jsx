704bcb72b4df143a855c6b7562b7778a44674eec
Joshua Cavell <xclusive36@gmail.com>
2023-11-13 15:05:05 -0500

    Update routing to add wildcard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,35 +36,51 @@ setupIonicReact();
 
 const App = () => (
   <IonApp>
+    {/* IonRouterOutlet is used to define the routes for the app */}
     <IonReactRouter>
+      {/* BlogProvider is used to provide the blog context data to the app */}
       <BlogProvider>
+        {/* IonRouterOutlet renders the pages based on the path */}
         <IonRouterOutlet>
-          <Route exact path="/home">
-            <Home />
-          </Route>
+          {/* Route / redirects to /home */}
           <Route exact path="/">
             <Redirect to="/home" />
           </Route>
+          {/* Route /home loads the Home component */}
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          {/* Route /about loads the About component */}
           <Route exact path="/about">
             <About />
           </Route>
+          {/* Route /projects loads the Projects component */}
           <Route exact path="/projects">
             <Projects />
           </Route>
+          {/* Route /contact loads the Contact component */}
           <Route exact path="/contact">
             <Contact />
           </Route>
+          {/* Route /resume loads the Resume component */}
           <Route exact path="/resume">
             <Resume />
           </Route>
+          {/* Route //litestep-ios-app-privacy-policy loads the AppPrivacy component */}
           <Route exact path="/litestep-ios-app-privacy-policy">
             <AppPrivacy />
           </Route>
+          {/* Route //litestep-ios-app-terms-of-use loads the AppTOS component */}
           <Route exact path="/litestep-ios-app-terms-of-use">
             <AppTOS />
           </Route>
+          {/* Route /blog/:blogSlug loads the blog component with the variable as blogSlug */}
           <Route path="/blog/:blogSlug" exact={true}>
             <Blog />
+          </Route>
+          {/* wildcard, any non matching route gets redirected to /home */}
+          <Route>
+            <Redirect to="/home" />
           </Route>
         </IonRouterOutlet>
       </BlogProvider>
