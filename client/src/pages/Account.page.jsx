dcfbdbef934fe29c7fa645fb5b499d7282dfb146
Joshua Cavell <xclusive36@gmail.com>
2023-12-04 19:17:43 -0500

    Restrict Account page to logged in users

--- client/src/pages/Account.page.jsx
+++ client/src/pages/Account.page.jsx
@@ -22,13 +22,20 @@ import Auth from "../utils/auth";
 import PageComponent from "../components/Page.component";
 import { checkmarkOutline, hourglassOutline } from "ionicons/icons";
 
-import "./Account.styles.css";
 import {
   QUERY_APPROVED_BLOGS,
   QUERY_MY_UNAPPROVED_BLOGS,
 } from "../utils/queries";
 
+import "./Account.styles.css";
+
 const AccountPage = () => {
+  const loginStatus = Auth.loggedIn();
+  useEffect(() => {
+    // redirect to login if not logged in
+    loginStatus || window.location.assign("/home");
+  }, [loginStatus]);
+
   // query for blogs waiting approval
   const {
     loading: loadingUnapproved,
@@ -87,156 +94,160 @@ const AccountPage = () => {
 
   return (
     <PageComponent>
-      <div className="home-container">
-        <h1 className="ion-text-center">Account</h1>
-        <IonButton expand="full" fill="clear">Create a new blog</IonButton>
-        <IonGrid>
-          <IonRow>
-            <IonCol sizeXs="12" sizeSm="6">
-              <IonCard>
-                <IonCardHeader>
-                  <IonCardTitle>Blogs waiting approval:</IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent>
-                  <IonSearchbar />
-                  <IonList>
-                    <IonListHeader>
-                      <IonLabel>Name:</IonLabel>
-                      <IonText>Approved:</IonText>
-                    </IonListHeader>
-                    {isUnapprovedLoading && (
-                      <IonItem>
-                        <IonLabel className="ion-text-wrap">
-                          <div>Loading...</div>
-                        </IonLabel>
-                      </IonItem>
-                    )}
-                    {unapprovedBlogs.map((blog) => (
-                      <IonItem key={blog._id}>
-                        <IonLabel className="ion-text-wrap">
-                          <a
-                            href={`/blog/${blog._id}`}
-                            className="blog-item-link"
-                          >
-                            {blog.title}
-                          </a>
-                          <div className="submitted-on">
-                            Submitted: {blog.createdAt}
-                          </div>
-                        </IonLabel>
-                        <IonText>
-                          <IonIcon icon={hourglassOutline} color="dark" />
-                        </IonText>
-                      </IonItem>
-                    ))}
-                    {errorUnapproved && (
-                      <IonItem>
-                        <IonLabel className="ion-text-wrap">
-                          <div className="blog-item-link">
-                            Error loading blogs
-                          </div>
-                          <div>{errorUnapproved.message}</div>
-                        </IonLabel>
-                      </IonItem>
-                    )}
-                    {unapprovedBlogs.length === 0 && !errorUnapproved ? (
-                      !isUnapprovedLoading && (
+      {loginStatus && (
+        <div className="home-container">
+          <h1 className="ion-text-center">Account</h1>
+          <IonButton expand="full" fill="clear">
+            Create a new blog
+          </IonButton>
+          <IonGrid>
+            <IonRow>
+              <IonCol sizeXs="12" sizeSm="6">
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>Blogs waiting approval:</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <IonSearchbar />
+                    <IonList>
+                      <IonListHeader>
+                        <IonLabel>Name:</IonLabel>
+                        <IonText>Approved:</IonText>
+                      </IonListHeader>
+                      {isUnapprovedLoading && (
                         <IonItem>
                           <IonLabel className="ion-text-wrap">
-                            <div>No blogs waiting approval</div>
+                            <div>Loading...</div>
                           </IonLabel>
                         </IonItem>
-                      )
-                    ) : (
-                      <IonItem lines="none" className="ion-text-center">
-                        <IonLabel>
-                          <form onSubmit={handleLoadMoreUnapproved}>
-                            <IonButton type="submit" expand="block">
-                              <IonText className="small">Load More</IonText>
-                            </IonButton>
-                          </form>
-                          <div className="submitted-on">5 of 10</div>
-                        </IonLabel>
-                      </IonItem>
-                    )}
-                  </IonList>
-                </IonCardContent>
-              </IonCard>
-            </IonCol>
-            <IonCol>
-              <IonCard>
-                <IonCardHeader>
-                  <IonCardTitle>Active blogs:</IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent>
-                  <IonSearchbar />
-                  <IonList>
-                    <IonListHeader>
-                      <IonLabel>Name:</IonLabel>
-                      <IonText>Approved:&nbsp;&nbsp;</IonText>
-                    </IonListHeader>
-                    {isApprovedLoading && (
-                      <IonItem>
-                        <IonLabel className="ion-text-wrap">
-                          <div>Loading...</div>
-                        </IonLabel>
-                      </IonItem>
-                    )}
-                    {approvedBlogs.map((blog) => (
-                      <IonItem key={blog._id}>
-                        <IonLabel className="ion-text-wrap">
-                          <a
-                            href={`/blog/${blog._id}`}
-                            className="blog-item-link"
-                          >
-                            {blog.title}
-                          </a>
-                          <div className="submitted-on">
-                            Submitted: {blog.createdAt}
-                          </div>
-                        </IonLabel>
-                        <IonText>
-                          <IonIcon icon={checkmarkOutline} color="success" />
-                        </IonText>
-                      </IonItem>
-                    ))}
-                    {errorApproved && (
-                      <IonItem>
-                        <IonLabel className="ion-text-wrap">
-                          <div className="blog-item-link">
-                            Error loading blogs
-                          </div>
-                          <div>{errorApproved.message}</div>
-                        </IonLabel>
-                      </IonItem>
-                    )}
-                    {approvedBlogs.length === 0 && !errorApproved ? (
-                      !isApprovedLoading && (
+                      )}
+                      {unapprovedBlogs.map((blog) => (
+                        <IonItem key={blog._id}>
+                          <IonLabel className="ion-text-wrap">
+                            <a
+                              href={`/blog/${blog._id}`}
+                              className="blog-item-link"
+                            >
+                              {blog.title}
+                            </a>
+                            <div className="submitted-on">
+                              Submitted: {blog.createdAt}
+                            </div>
+                          </IonLabel>
+                          <IonText>
+                            <IonIcon icon={hourglassOutline} color="dark" />
+                          </IonText>
+                        </IonItem>
+                      ))}
+                      {errorUnapproved && (
                         <IonItem>
                           <IonLabel className="ion-text-wrap">
-                            <div>No active blogs</div>
+                            <div className="blog-item-link">
+                              Error loading blogs
+                            </div>
+                            <div>{errorUnapproved.message}</div>
                           </IonLabel>
                         </IonItem>
-                      )
-                    ) : (
-                      <IonItem lines="none" className="ion-text-center">
-                        <IonLabel>
-                          <form onSubmit={handleLoadMoreApproved}>
-                            <IonButton type="submit" expand="block">
-                              <IonText className="small">Load More</IonText>
-                            </IonButton>
-                          </form>
-                          <div className="submitted-on">5 of 10</div>
-                        </IonLabel>
-                      </IonItem>
-                    )}
-                  </IonList>
-                </IonCardContent>
-              </IonCard>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </div>
+                      )}
+                      {unapprovedBlogs.length === 0 && !errorUnapproved ? (
+                        !isUnapprovedLoading && (
+                          <IonItem>
+                            <IonLabel className="ion-text-wrap">
+                              <div>No blogs waiting approval</div>
+                            </IonLabel>
+                          </IonItem>
+                        )
+                      ) : (
+                        <IonItem lines="none" className="ion-text-center">
+                          <IonLabel>
+                            <form onSubmit={handleLoadMoreUnapproved}>
+                              <IonButton type="submit" expand="block">
+                                <IonText className="small">Load More</IonText>
+                              </IonButton>
+                            </form>
+                            <div className="submitted-on">5 of 10</div>
+                          </IonLabel>
+                        </IonItem>
+                      )}
+                    </IonList>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+              <IonCol>
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>Active blogs:</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <IonSearchbar />
+                    <IonList>
+                      <IonListHeader>
+                        <IonLabel>Name:</IonLabel>
+                        <IonText>Approved:&nbsp;&nbsp;</IonText>
+                      </IonListHeader>
+                      {isApprovedLoading && (
+                        <IonItem>
+                          <IonLabel className="ion-text-wrap">
+                            <div>Loading...</div>
+                          </IonLabel>
+                        </IonItem>
+                      )}
+                      {approvedBlogs.map((blog) => (
+                        <IonItem key={blog._id}>
+                          <IonLabel className="ion-text-wrap">
+                            <a
+                              href={`/blog/${blog._id}`}
+                              className="blog-item-link"
+                            >
+                              {blog.title}
+                            </a>
+                            <div className="submitted-on">
+                              Submitted: {blog.createdAt}
+                            </div>
+                          </IonLabel>
+                          <IonText>
+                            <IonIcon icon={checkmarkOutline} color="success" />
+                          </IonText>
+                        </IonItem>
+                      ))}
+                      {errorApproved && (
+                        <IonItem>
+                          <IonLabel className="ion-text-wrap">
+                            <div className="blog-item-link">
+                              Error loading blogs
+                            </div>
+                            <div>{errorApproved.message}</div>
+                          </IonLabel>
+                        </IonItem>
+                      )}
+                      {approvedBlogs.length === 0 && !errorApproved ? (
+                        !isApprovedLoading && (
+                          <IonItem>
+                            <IonLabel className="ion-text-wrap">
+                              <div>No active blogs</div>
+                            </IonLabel>
+                          </IonItem>
+                        )
+                      ) : (
+                        <IonItem lines="none" className="ion-text-center">
+                          <IonLabel>
+                            <form onSubmit={handleLoadMoreApproved}>
+                              <IonButton type="submit" expand="block">
+                                <IonText className="small">Load More</IonText>
+                              </IonButton>
+                            </form>
+                            <div className="submitted-on">5 of 10</div>
+                          </IonLabel>
+                        </IonItem>
+                      )}
+                    </IonList>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
+      )}
     </PageComponent>
   );
 };
