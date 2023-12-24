269d9e18a3e055312ce641e3d14eb23e92162014
Joshua Cavell <xclusive36@gmail.com>
2023-12-23 19:30:44 -0500

    Add Privacy and Terms pages content

--- client/src/pages/Privacy.page.jsx
+++ client/src/pages/Privacy.page.jsx
@@ -4,7 +4,87 @@ const PrivacyPage = () => {
   return (
     <PageComponent>
       <div className="home-container">
-        <h1>Privacy Page</h1>
+        <h1>Privacy Policy</h1>
+        <p>
+          This Privacy Policy explains how we collect, process, and store your
+          personal data. We treat your personal data in accordance with our
+          obligations under the law.
+        </p>
+        <h2>Information we collect</h2>
+        <p>
+          We collect information to provide better services to our users and
+          improve our business.
+        </p>
+        <h2>How we use your information</h2>
+        <p>
+          We use the information we collect in various ways, including to
+          provide our services, communicate with you, troubleshoot issues,
+          secure against fraud and abuse, improve and update our services,
+          analyze how people use our services, serve personalized advertising,
+          and as required by law or necessary for safety and integrity.
+        </p>
+        <h2>How we share your information</h2>
+        <p>
+          We do not sell your personal data to third parties. We share your
+          information only in the ways that are described in this Privacy
+          Policy, and only with parties who adopt appropriate confidentiality
+          and security measures.
+        </p>
+        <h2>Cookies and Tracking</h2>
+        <p>
+          We use cookies and similar tracking technologies to track the activity
+          on our service and we hold certain information.
+        </p>
+        <h2>Data Retention</h2>
+        <p>
+          We will retain your personal data only for as long as is necessary for
+          the purposes set out in this Privacy Policy.
+        </p>
+        <h2>User Rights</h2>
+        <p>
+          You have the right to access, update or to delete the personal
+          information we have on you.
+        </p>
+        <h2>Security Measures</h2>
+        <p>
+          We use security measures to protect your personal information from
+          access by unauthorized persons and against unlawful processing,
+          accidental loss, destruction and damage.
+        </p>
+        <h2>Contact Information</h2>
+        <p>
+          If you have any questions about this Privacy Policy, please contact
+          us.
+        </p>
+        <h2>User Rights</h2>
+        <p>
+          You have the right to access, update or to delete the personal
+          information we have on you.
+        </p>
+        <h2>Security Measures</h2>
+        <p>
+          We use security measures to protect your personal information from
+          access by unauthorized persons and against unlawful processing,
+          accidental loss, destruction and damage.
+        </p>
+        <h2>Contact Information</h2>
+        <p>
+          If you have any questions about this Privacy Policy, please contact
+          us.
+        </p>
+        <h2>Children&apos;s Privacy</h2>
+        <p>
+          Our website is not intended for children under the age of 13. We do
+          not knowingly collect personal information from children under 13. If
+          we learn that we have collected personal information of a child under
+          13, we will take steps to delete such information from our files as
+          soon as possible.
+        </p>
+        <h2>Updates to this policy</h2>
+        <p>
+          We may change this Privacy Policy from time to time. We will post any
+          changes to this Privacy Policy on this page.
+        </p>
       </div>
     </PageComponent>
   );
--- client/src/pages/Terms.page.jsx
+++ client/src/pages/Terms.page.jsx
@@ -4,7 +4,89 @@ const TermsPage = () => {
   return (
     <PageComponent>
       <div className="home-container">
-        <h1>Terms Page</h1>
+        <h1>Terms of Use</h1>
+        <p>
+          Welcome to Our Website. If you continue to browse and use this
+          website, you are agreeing to comply with and be bound by the following
+          terms and conditions of use, which together with our privacy policy
+          govern Our Company&apos;s relationship with you in relation to this
+          website.
+        </p>
+        <h2>Use of the Website</h2>
+        <p>
+          By accessing the website, you warrant and represent to the website
+          owner that you are legally entitled to do so and to make use of
+          information made available via the website.
+        </p>
+        <h2>Trademarks</h2>
+        <p>
+          The trademarks, names, logos and service marks (collectively
+          &quot;trademarks&quot;) displayed on this website are registered and
+          unregistered trademarks of the website owner. Nothing contained on
+          this website should be construed as granting any license or right to
+          use any trademark without the prior written permission of the website
+          owner.
+        </p>
+        <h2>External links</h2>
+        <p>
+          External links may be provided for your convenience, but they are
+          beyond the control of the website owner and no representation is made
+          as to their content. Use or reliance on any external links and the
+          content thereon provided is at your own risk.
+        </p>
+        <h2>Warranties</h2>
+        <p>
+          The website owner makes no warranties, representations, statements or
+          guarantees (whether express, implied in law or residual) regarding the
+          website.
+        </p>
+        <h2>Compliance with Laws</h2>
+        <p>
+          As a condition of your access and use, you agree to abide by all
+          applicable local, state, national, and international laws and
+          regulations in your use of the website. You also agree that you will
+          not violate any rights of any third parties, and that you will not
+          misuse the content or services in any way.
+        </p>
+        <h2>User Responsibilities</h2>
+        <p>
+          As a user, you agree not to misuse the services or content, or to
+          assist or encourage any third party to do so. We reserve the right to
+          enforce this provision, including but not limited to terminating your
+          access to the service.
+        </p>
+        <h2>Dispute Resolution</h2>
+        <p>
+          Any disputes arising out of or relating to the use of the website or
+          these Terms of Use will be resolved in accordance with the laws of
+          your jurisdiction.
+        </p>
+        <h2>Limitation of Liability</h2>
+        <p>
+          To the maximum extent permitted by law, the website owner shall not be
+          liable for any indirect, incidental, special, consequential or
+          punitive damages, or any loss of profits or revenues, whether incurred
+          directly or indirectly, or any loss of data, use, goodwill, or other
+          intangible losses, resulting from your access to or use of or
+          inability to access or use the website.
+        </p>
+        <h2>Termination of Service</h2>
+        <p>
+          We reserve the right to terminate or restrict your access to the
+          website, without notice, for any or no reason whatsoever.
+        </p>
+        <h2>Intellectual Property Rights</h2>
+        <p>
+          All intellectual property rights in the website and the content and
+          services provided on the website are owned by us and/or our licensors.
+          You have no right to use such intellectual property rights unless you
+          have our prior written consent.
+        </p>
+        <h2>Updates to this policy</h2>
+        <p>
+          We may change these Terms of Use from time to time. We will post any
+          changes to these Terms of Use on this page.
+        </p>
       </div>
     </PageComponent>
   );
