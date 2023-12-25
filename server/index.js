a2adbe465a73ca0dab05518dbce49738a004f416
Joshua Cavell <xclusive36@gmail.com>
2023-12-25 14:13:45 -0500

    Update content security policy headers

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use(
           // "apollo-server-landing-page.cdn.apollographql.com",
           "*", // allow all sources to load images (not recommended)
         ],
-        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+        scriptSrc: ["self", ajax.cloudflare.com],
         manifestSrc: [
           `'self'`,
           "apollo-server-landing-page.cdn.apollographql.com",
