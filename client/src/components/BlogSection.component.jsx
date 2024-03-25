77914ade365952a0ac07da33dcf18952a7d940ed
Joshua Cavell <xclusive36@gmail.com>
2024-03-25 11:33:01 -0400

    Remove context from BlogSection component because its not currently in use

--- client/src/components/BlogSection.component.jsx
+++ client/src/components/BlogSection.component.jsx
@@ -1,12 +1,9 @@
 import { IonCol, IonGrid, IonRow } from "@ionic/react";
 import BlogItemComponent from "./BlogItem.component";
-import { BlogContext } from "../context/blogContext";
-import { useContext } from "react";
 import { QUERY_APPROVED_BLOGS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 
 const BlogSectionComponent = () => {
-  const { BlogArray } = useContext(BlogContext);
   const { loading, data } = useQuery(QUERY_APPROVED_BLOGS);
 
   return (
@@ -21,8 +18,7 @@ const BlogSectionComponent = () => {
               sizeMd="6"
               sizeSm="12"
               sizeXs="12"
-              key={blog._id}
-            >
+              key={blog._id}>
               <BlogItemComponent
                 blog={blog}
                 showIntro={false}
@@ -31,15 +27,6 @@ const BlogSectionComponent = () => {
             </IonCol>
           ))
         )}
-        {BlogArray.map((blog) => (
-          <IonCol sizeLg="4" sizeMd="6" sizeSm="12" sizeXs="12" key={blog.id}>
-            <BlogItemComponent
-              blog={blog}
-              showIntro={false}
-              showContent={false}
-            />
-          </IonCol>
-        ))}
       </IonRow>
     </IonGrid>
   );
